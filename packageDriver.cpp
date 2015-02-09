631ff1767adb24bb4cab216209fe04568490a4e0
Charles Shinaver <bshinaver@gmail.com>
2015-02-09 17:57:53 -0500

    Package driver now prints Twodaypackage and Overnight package above for readability'

--- packageDriver.cpp
+++ packageDriver.cpp
@@ -12,7 +12,10 @@ int main()
 {
     TwoDayPackage p("Charles", "Address", "46032", 50, 3.30, 5);
     OvernightPackage o("Jared", "Address", "11369", 25, 5.30, 4.20);
+
+    cout << "Two Day Package" << endl;
     cout << p << endl;
+    cout << "Overnight Package" << endl;
     cout << o << endl;
 }
 
