d747f4872660af4ed2a993d5c393b3911abeff54
Charles Shinaver <bshinaver@gmail.com>
2015-02-09 15:01:46 -0500

    Removed base class include from packageDriver since already included

--- packageDriver.cpp
+++ packageDriver.cpp
@@ -5,12 +5,11 @@
 */
 
 #include <iostream>
-#include "Package.h"
 #include "TwoDayPackage.h"
 
 int main()
 {
     TwoDayPackage p("Charles", "Address", "46032", 50, 3.30, 5);
-    cout << p << endl;
+    cout << p;
 }
 
